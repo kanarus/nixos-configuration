8378a492ac6ef82664d3f04518b370dc8cea4190
kanarus <mail@kanarus.dev>
2026-03-30 22:35:11 +0900

    firefox policies

--- home/browser/default.nix
+++ home/browser/default.nix
@@ -2,5 +2,26 @@
 {
   programs.firefox = {
     enable = true;
+    policies = {
+      "FirefoxHome" = {
+        "Search" = false;
+        "TopSites" = false;
+        "SponsoredTopSites" = false;
+        "Highlights" = false;
+        "Pocket" = false;
+        "SponsoredPocket" = false;
+        "Stories" = false;
+        "SponsoredStories" = false;
+        "Snippets" = false;
+        "Locked" = false;
+      };
+      "Preferences" = {
+        "toolkit.scrollbox.verticalScrollDistance" = 5;
+        "general.smoothscroll.lines.durationMaxMs" = 600;
+        "general.smoothscroll.lines.durationMinMs" = 400;
+        "general.smoothscroll.pages.durationMaxMs" = 500;
+        "general.smoothscroll.currentVelocityWeighting" = 0;
+      };
+    };
   };
 }
