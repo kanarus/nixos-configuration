3606fb03198a58fc6537cdf1d828709fd6869d3c
kanarus <mail@kanarus.dev>
2026-04-01 15:48:37 +0900

    sddm backgroundFillMode: aspect -> fill

--- system/sddm.nix
+++ system/sddm.nix
@@ -16,7 +16,7 @@ let
       substituteInPlace where_is_my_sddm_theme/theme.conf \
         --replace 'background='                     'background=background.png' \
         --replace 'backgroundFill=#000000'          'backgroundFill=' \
-        --replace 'backgroundFillMode=aspect'       'backgroundFillMode=strech'\
+        --replace 'backgroundFillMode=aspect'       'backgroundFillMode=fill'\
         --replace 'font=monospace'                  'font="UDEV Gothic 35NF"' \
         --replace 'helpFont=monospace'              'helpFont="UDEV Gothic 35NF"' \
         --replace 'passwordInputCursorVisible=true' 'passwordInputCursorVisible=false' \
