1b438f517f6528cb3eaafe3cae56bbf4151f6d41
kanarus <mail@kanarus.dev>
2026-04-13 03:26:55 +0900

    firefox: migrate policies.Preferences to profiles.<name>.settings

--- home/browser/default.nix
+++ home/browser/default.nix
@@ -18,14 +18,6 @@
         "Snippets" = false;
         "Locked" = false;
       };
-      "Preferences" = {
-        "browser.translations.automaticallyPopup" = false;
-        "toolkit.scrollbox.verticalScrollDistance" = 5;
-        "general.smoothscroll.lines.durationMaxMs" = 600;
-        "general.smoothscroll.lines.durationMinMs" = 400;
-        "general.smoothscroll.pages.durationMaxMs" = 500;
-        "general.smoothscroll.currentVelocityWeighting" = 0;
-      };
       "ExtensionSettings" = {
         "*" = {
           "installation_mode" = "blocked"; # block any other addons not listed below
@@ -34,6 +26,19 @@
           "installation_mode" = "force_installed";
           "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
         };
+      };
+    };
+    profiles."kanarus" = {
+      settings = {
+        "sidebar.revamp" = true;
+        "sidebar.verticalTabs" = true;
+        "browser.tabs.groups.enabled" = true;
+        "browser.translations.automaticallyPopup" = false;
+        "toolkit.scrollbox.verticalScrollDistance" = 5;
+        "general.smoothscroll.lines.durationMaxMs" = 600;
+        "general.smoothscroll.lines.durationMinMs" = 400;
+        "general.smoothscroll.pages.durationMaxMs" = 500;
+        "general.smoothscroll.currentVelocityWeighting" = 0;
       };
     };
   };
