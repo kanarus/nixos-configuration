0156d23cefb9a4ccefe52b0750dff0d09e3e09c8
kanarus <mail@kanarus.dev>
2026-07-20 00:15:14 +0900

    fcitx5-mozc -> fcitx5-mozc-ut

--- home/i18n/default.nix
+++ home/i18n/default.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
-        fcitx5-mozc
+        fcitx5-mozc-ut
         fcitx5-fluent
       ];
       settings = {
